1bc3d3b9a6a59ca04549554b6cb99883fed6c598
Rajput <sairajput@deloitte.com>
2020-03-19 17:20:37 +0530

    commit

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='login'>
+    <div class="login_logo_div">
+      <img class='login_logo' alt='HUX logo' src='../assets/hux_logo.jpg' />
+    </div>
+    <div class="login_welcome_div">
+      <h2 class="login_welcome">Welcome to HUX Engage <br> Model Selection</h2>
+    </div>
+    <div class="login_form_div">
+      <form class="form-group">
+        <div class="login_form_div1">
+          <input type="text" name="username" class="form-control" placeholder="Username" required>
+        </div>
+        <div class="login_form_div1">
+          <input type="password" class="form-control" placeholder="Password" required>
+        </div>
+        <div class="login_submit_div">
+          <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+    </div>
+  </div>
+</template>
